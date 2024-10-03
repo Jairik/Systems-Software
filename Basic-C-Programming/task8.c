d0cf353cdf3677a0936cc6c1ca3c29528b8c8451
JJ McCauley <jairikmc@gmail.com>
2024-10-03 00:19:33 -0400

    Completed Task 8 using Dup2 Command

--- Basic-C-Programming/task8.c
+++ Basic-C-Programming/task8.c
@@ -12,12 +12,6 @@ void noooError(char *str){
 	printf("%s", str);
 }
 
-//Convert int to char
-char int_to_char(int digit){
-	//return digit + '\0';
-	return digit;
-}
-
 //Takes in two arguments: input file (1) and output file (2)
 int main(int argc, char*argv[]){
 	
@@ -26,7 +20,7 @@ int main(int argc, char*argv[]){
 	}
 
 	char buffer[1];
-	char ascii_str[10];  //Hold the ascii string value
+	int ascii;
 	int nbyte;
 
 	//Get input and output files
@@ -42,14 +36,18 @@ int main(int argc, char*argv[]){
 	int oFile = open(output_file, O_WRONLY | O_CREAT, 0666);
 	if(oFile < 0)
 		noooError("Error opening output file");
+	
+	//Redirect standard output to output file
+	if (dup2(oFile, STDOUT_FILENO) == -1) {
+        	noooError("Error Redirecting File Output");
+    	}
 
 	//Iterate through the rFile, reading, converting to Ascii, then writing to oFile
 	while((nbyte = read(rFile, buffer, 1)) > 0) {
-		buffer[0] = int_to_char((int)buffer[0]);
-		if((write(oFile, buffer, 1)) < 0){
-			noooError("Error writing to file");
-		}
+		ascii = (int)buffer[0];
+		printf("%i", ascii);
 	}
+	printf("\n");
 
 	exit(0);
 }
--- Basic-C-Programming/task9.c
+++
@@ -1,8 +0,0 @@
-/*Lab 3 - Task 9
- * JJ McCauley
- * Task: Decoding program to convert output of task8 to original output file */
-
-int main(int argc, char*argv[]){
-	
-
-}
