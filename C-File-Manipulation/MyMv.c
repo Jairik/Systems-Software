53a8a04c234501c75d9cc12dd75cf15e16cfaaaf
Jairik <jairikmc@gmail.com>
2024-10-21 00:25:40 -0400

    Completed MyMv function
    A little rough around the edges but it gets the job done

--- C-File-Manipulation/MyMv.c
+++ C-File-Manipulation/MyMv.c
@@ -9,9 +9,12 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
+// Debugging libraries
+//#include <errno.h> 
+//#include <string.h>
 
 void pError(char *msg){
-	printf("%s", msg);
+	printf("%s\n", msg);
 	exit(1);
 }
 
@@ -22,34 +25,35 @@ char *getHomeDir(){
 	struct passwd *pwd = getpwuid(uid);  // Get struct that contains user information
 	return pwd->pw_dir;  // Return user's home directory
 }
+*/
 
-// Given a path, returns a c-string of the full path with home directory
-char *getFullPath(char *dir){
-	char *homeDir = getHomeDir();  // Get home directory
+// Given a path and file name, returns c-string of new path with file name
+char *getFullPath(char *dir, char *file){
 	char *newPath;
-	if((newPath = (char *)malloc((strlen(homeDir) + strlen(dir) + 1) * sizeof(char))) == NULL){
+	if((newPath = (char *)malloc((strlen(dir) + strlen(file) + 3) * sizeof(char))) == NULL){
 		pError("Error allocating memory");
 	}
 
 	// Getting sizes of different c-strings
 	int dirSize = strlen(dir);
-	int homeDirSize = strlen(homeDir);
+	int fileSize = strlen(file);  // Poor name hindsight
 	int newPathSize = strlen(newPath);
 	
 	// Adding the home dir to new path dir
-	for(int i = 0; i < homeDirSize; i++){
-		newPath[i] = homeDir[i];
+	for(int i = 0; i < dirSize; i++){
+		newPath[i] = dir[i];
 	}
-	for(int j = 0; j < dirSize; j++){
-		newPath[j+homeDirSize] = dir[j];
+	newPath[dirSize] = '/';
+	for(int j = 0; j < fileSize; j++){
+		newPath[j+dirSize+1] = file[j];
 	}
 
 	// add \0 to end of newPath
-	newPath[homeDirSize + dirSize] = '\0';
+	newPath[dirSize + fileSize + 1] = '\0';
 
 	// Return the new c-string
 	return newPath;
-} */
+}
 
 
 // Parameters: file name and path to directory
@@ -59,25 +63,27 @@ int main(int argc, char* argv[]){
 	// Ensure valid number of parameters is provided
 	if(argc != 3){pError("Invalid number of arguments");}
 
-	char *file = argv[1], *path = argv[2];
+	char *file = argv[1], *path = argv[2], *fName = path;
 
-	// Ensure that the file and path exists
-	if((access(argv[1], R_OK)) == -1 || ()access(argv[2], R_OK)) == -1){pError("File does not exist");}
-	
 	// Check if the given path is a directory
 	struct stat path_stat;
-	if((stat(path, &path_stat)) == -1){pError("Stat error");}
-	if(S_ISDIR(path_stat.st_mode)){
+	if((stat(path, &path_stat)) == -1){
+		// File does not exists - will be made with file name in link
+		isDir = 0;
+	}  
+	else if(S_ISDIR(path_stat.st_mode)){
 		isDir = 1;
+		path = getFullPath(path, file);
+		//printf("New Path: %s\n", path);
+	
 	}
-
-	//If given path is a directory, create new file to link to
-	if(dir == 1){
-
+	else { 
+		pError("Error - cannot overwrite file");  // Can be taken out if not desired
 	}
 
 	// Moving file to new spot 
 	if((link(file, path)) == -1){
+		//printf("Error: %s\n", strerror(errno));
 		pError("Error linking file");
 	}
 
