0897e4d3b22bf8538ba97f3939f0b4449c4b1aa0
Jairik <jairikmc@gmail.com>
2024-10-21 21:54:41 -0400

    Completed task2
    I cant beleive that actually worked

---
+++ Advanced-C-Topics/getenv.c
@@ -0,0 +1,48 @@
+/* Lab 5-6 Task 1
+ * JJ McCauley
+ * Task: Create getenv function from scratch */
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>  // Used for strlen function
+
+extern char **environ;
+
+// Print error function
+void e(char *msg){
+	printf("%s\n", msg);
+	exit(1);
+}
+
+char *mygetenv(const char *name){
+
+	int nameLen = strlen(name), j = 0; 
+	
+	// Get reference to environment variables
+	char **p = environ;
+	
+	// Iterate through all environment variables, returning the one equal to name
+	for(int i = 0; p[i] != NULL; i++){
+		// Determine if current c-string is the one we are looking for
+		j = 0;
+		while(name[j] != '\0' && p[i][j] != '\0' && name[j] == p[i][j]){j++;}
+		if(name[j] == '\0' && p[i][j] == '='){
+			return p[i] + j + 1;
+		}
+	}
+	return NULL;  // If not found, return NULL
+}
+
+
+
+// Test getenv function
+void main(){
+	char *path = mygetenv("PATH");
+	printf("Path: %s\n\n", path);
+	
+	char *home = mygetenv("HOME");
+	printf("Home: %s\n\n", home);
+	
+	exit(0);
+}
---
+++ Advanced-C-Topics/task2.c
@@ -0,0 +1,49 @@
+/* Lab 5-6 Task 2
+ * JJ McCauley
+ * Task: Read the utmp file and count number of USER_PROCESS records */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <utmp.h>
+
+
+// Print error function
+void pError(char *msg){
+	printf("%s\n", msg);
+	exit(1);
+}
+
+
+// Opens utmp file and returns file descriptor
+int openUtmpFile(struct utmp ut){
+	int fd = open(_PATH_UTMP, O_RDONLY);
+	if(fd == -1){pError("Error opening utmp file");}
+	return fd;
+}
+
+
+// use openUtmp function & count number records in USER_PROCESS
+void main(){
+	struct utmp ut;
+	const int utmp_size = sizeof(struct utmp);
+	// Create buffer of type utmp to hold each individual record while reading
+	struct utmp buffer[utmp_size];
+	// Open utmp file
+	int utmpFd = openUtmpFile(ut), count = 0;
+	if(utmpFd < 0){pError("Error opening utmp file");}
+
+	// Loop through contents to find USER_PROCESS
+	while((read(utmpFd, buffer, utmp_size)) == utmp_size){
+		if(buffer->ut_type == USER_PROCESS){
+			//Print the username and increment count
+			printf("Username: %s\n", buffer[0].ut_user);
+			count++;
+		}
+	}
+	
+	// Print count and exit program
+	printf("Total number of user_process records: %d\n", count);
+	exit(0);
+}
