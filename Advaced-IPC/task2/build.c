afd05ee7725fc5b4d2d4457520c5f86b8856dbdc
Jairik <jairikmc@gmail.com>
2024-11-30 20:50:50 -0500

    Began skeletons of last task

---
+++ Advaced-IPC/task2/build.c
@@ -0,0 +1,13 @@
+/* Lab 10 - Task 2
+ * JJ McCauley
+ * Task: Create shared memory space for saving 5 integers (array) */
+
+// Create struct to hold stored data
+struct memType {
+	int nums[5];
+	// Flags here
+};
+
+int main(){
+
+}
---
+++ Advaced-IPC/task2/producer.c
@@ -0,0 +1,12 @@
+/* Lab 10 - Task 2
+ * JJ McCauley
+ * Task: Acts as producer in Producer-Consumer problem */
+
+struct memType{
+	int nums[5];
+	// Flags here
+};
+
+int main(){
+
+}
