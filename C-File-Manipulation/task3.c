9d076822dce2c088b477e796d5ec870acb62c0ee
Jairik <jairikmc@gmail.com>
2024-12-09 16:19:32 -0500

    Removed Debugging Statements

--- C-File-Manipulation/task3.c
+++ C-File-Manipulation/task3.c
@@ -24,23 +24,17 @@ int palind(int fd1, int fd2){
 	int middle = ((lseek(fd2, 0, SEEK_END))/2);  // Setting offset middle
 	int offsetBackward = lseek(fd2, -2, SEEK_END);	// Setting fd2 to read backward
 	int cForward, cBackward, counter = 0;
-	printf("Offset center: %d\n", middle);
-	printf("Scanning for palindrome...\n");
 	while(counter <= middle){
 		// Read forward
 		offsetForward = lseek(fd1, counter, SEEK_SET);
-		printf("Current forward offset: %d\n", counter);
 		if((cForward = read(fd1, &buf1, BSIZE)) < 0){pError("Error reading file (fd1)");}
-		printf("Reading %c from fd1 buf\n", buf1);
 		
 		// Read backward
 		offsetBackward = lseek(fd2, (-1 * (counter+2)), SEEK_END);
-		printf("Current backwards offset: %d\n", offsetBackward);
 		if((cBackward = read(fd2, &buf2, BSIZE)) < 0){pError("Error reading file (fd2)");}
-		printf("Reading %c from fd2 buf\n", buf2);
 		
 		// Compare characters and increment counter for next iteration
-		if(buf1 != buf2){printf("Counterargument Found! %c vs %c\n", buf1, buf2); return 0;}  // Counterargument found, not palindrome
+		if(buf1 != buf2){printf(return 0;}  // Counterargument found, not palindrome
 		counter++;
 	}	
 	return 1;  // No counterarguments found, contents forms a palindrome
@@ -52,8 +46,7 @@ int main(int argc, char *argv[]){
 	int openFd, newfd, result;
 	if((openFd = open(argv[1], O_RDONLY, 0666)) < 0) {pError("Error opening file");}
 
-	//Create duplicate file descriptor
-	// --- ADD DUP TO MAKE WORK -- 
+	//Create duplicate file descriptor (Duplicate file descriptor, but shares the same offset)
 	if((newfd = dup(openFd)) == -1){pError("Error duplicating file descriptor");}
 	
 	//Call & test palind function
